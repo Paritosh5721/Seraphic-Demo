e58861e6cb9aae48a911919291745c2fd3c3cc28
Paritosh5721 <98382721+Paritosh5721@users.noreply.github.com>
2023-09-28 23:13:06 +0530

    started working

---
+++ Seraphic_Demo/Seraphic_Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seraphic_Demo
+//
+//  Created by Paritosh on 28/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
